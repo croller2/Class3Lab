6e1f04c40e321bd9621ac8db4a1d6468ba9e2536
croller2 <croller2@my.wctc.edu>
2017-02-01 10:31:28 -0600

    initial git push

---
+++ src/java/models/CalculatorService.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author chris
+ */
+public class CalculatorService {
+    private double width;
+    private double height;
+    
+    public CalculatorService(String width, String height){
+            setWidth(width);
+            setHeight(height);
+    }
+
+    public final double getWidth() {
+        return width;
+    }
+
+    public final void setWidth(String width) {
+        try
+        {
+            double input = Double.parseDouble(width);
+            if(input > 0){
+                this.width = input;
+            }
+        }catch(NumberFormatException e)
+        {
+            this.width = 0.0;
+        }
+    }
+
+    public final double getHeight() {
+        return height;
+    }
+
+    public final void setHeight(String height) {
+        try
+        {
+            double input = Double.parseDouble(height);
+            if(input > 0){
+                this.height = input;
+            }
+        }catch(NumberFormatException e)
+        {
+            this.height = 0.0;
+        }
+    }
+    
+    public final double calculateArea(){
+        return height*width;
+    }
+}
+
+
---
+++ web/rectangleResult.jsp
@@ -0,0 +1,23 @@
+<%-- 
+    Document   : rectangleResult
+    Created on : Feb 1, 2017, 8:22:25 AM
+    Author     : chris
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Class 3</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="row">
+            <div class="col-xs-4 col-xs-offset-4">
+                <h3>The area of your rectangle is : ${area}</h3    
+            </div>
+        </div>
+    </body> 
+</html>
